e84607e7fd02d69a9f487ec513754b69daae10b3
fefemf10 <dima.aldakimov@yandex.ru>
2025-04-24 17:51:55 +0300

    remove

--- src/api.ts
+++ src/api.ts
@@ -2,18 +2,7 @@ import ky from 'ky'
 import userManager from './oidc'
 
 const api = ky.create({
-  prefixUrl: `http://203.31.40.27:8080`,
-  hooks: {
-    beforeRequest: [
-      async (request) => {
-        const user = await userManager.getUser()
-        const accessToken = user?.access_token
-        if (accessToken) {
-          request.headers.set('Authorization', `Bearer ${accessToken}`)
-        }
-      },
-    ],
-  },
+  prefixUrl: `http://203.31.40.27:8080`
 })
 
 export default api
